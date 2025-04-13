9d6468be68b174045ceee0b8b99605769bccb952
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-04-13 12:34:22 +0300

    feat: resolve get power and polynom functions

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -75,8 +75,8 @@ function getArgumentsCount(funcs) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return (x) => x ** exponent;
 }
 
 /**
@@ -92,8 +92,15 @@ function getPowerFunction(/* exponent */) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  if (args.length === 0) {
+    return null;
+  }
+  return (x) => {
+    return args.reduce((acc, i, index) => {
+      return acc + i * x ** (args.length - index - 1);
+    }, 0);
+  };
 }
 
 /**
