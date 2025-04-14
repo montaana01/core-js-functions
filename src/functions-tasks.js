ce6569c3fd59161fd7c50fdb1d390fe0a0374c0f
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-04-14 14:12:38 +0300

    feat: resolve logger function

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -172,8 +172,17 @@ function retry(/* func, attempts */) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function log(...args) {
+    const functionName = func.name || 'anonymous';
+    const argsString = args.map((arg) => JSON.stringify(arg)).join(',');
+
+    logFunc(`${functionName}(${argsString}) starts`);
+    const result = func(...args);
+    logFunc(`${functionName}(${argsString}) ends`);
+
+    return result;
+  };
 }
 
 /**
