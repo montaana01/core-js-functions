de734230806c2f8586d200d523e4a7e19ba32dc9
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-04-14 19:00:35 +0300

    feat: resolve id generator function

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -231,8 +231,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let id = startFrom;
+  return () => {
+    const current = id;
+    id += 1;
+    return current;
+  };
 }
 
 module.exports = {
